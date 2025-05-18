868d484526ca9464a2ba010eb13f7c3c5726eb86
AngeloGagno <angagelogagno@gmail.com>
2025-05-17 22:14:22 -0300

    feat:funcao para ler e unir os arquivos csv da pasta

---
+++ code/src/extract/csv_cleaner.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import pandas as pd
+
+class GetFiles:
+    def __init__(self,file_name:str) -> None:
+        self.path = self.path(file_name)
+        self.folders = self.fetch_all_folders()
+
+    def path(self,file_name:str):
+        '''Retorna o caminho da pasta onde contem os arquivos csv'''
+        actual_path = Path.cwd()
+        return actual_path.parents[2] / file_name
+
+    def fetch_all_folders(self):
+        '''Acessa a pasta que contem os arquivos csv e retorna uma lista com todos'''
+        path = self.path
+        return [str(folder) for folder in path.rglob("*.csv")]
+            
+    def merge_and_delete_csvs(self):
+        '''Une os arquivos CSV contidos na pasta que contem os arquivos csv'''
+        csv_files = self.fetch_all_folders()
+        if not csv_files:
+            print("Nenhum arquivo CSV encontrado.")
+            return None
+        dfs = [pd.read_csv(file,sep=';') for file in csv_files]
+        df_final = pd.concat(dfs, ignore_index=True)
+
+        #for file in csv_files:
+        #    file.unlink()
+        return df_final
+
+
+print(GetFiles('dados por uf').merge_and_delete_csvs().iloc[:,61])
